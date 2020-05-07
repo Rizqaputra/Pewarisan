8ae8d2731dc6f2c9fb1dda452e72090c418f5fc9
Rizqaputra <rizqa.28@students.amikom.ac.id>
2020-05-07 21:11:03 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Pewarisan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih Printer!!!");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet/\n");
+
+            Console.Write("Nomor Printer [1..3] : ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else
+                printer = new LaserJet();
+
+            printer.show();
+            printer.print();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+public class Canon : PrinterWindows
+{
+    public override void show()
+    {
+        Console.WriteLine("Canon Display dimension : 9.5*12");
+    }
+
+    public override void print()
+    {
+        Console.WriteLine("Canon printer printing ...");
+    }
+}
+
+public class Epson : PrinterWindows
+{
+    public override void show()
+    {
+        Console.WriteLine("Epson Display dimension : 10*11");
+    }
+
+    public override void print()
+    {
+        Console.WriteLine("Epson printer printing ...");
+    }
+}
+public class LaserJet : PrinterWindows
+{
+    public override void show()
+    {
+        Console.WriteLine("Laserjet Display dimension : 12*12");
+    }
+
+    public override void print()
+    {
+        Console.WriteLine("LaserJet printer printing ...");
+    }
+}
+
+
